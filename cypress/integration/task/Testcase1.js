1ccf494184ca9f5617326f9e01a6e527ba16e19f
Dagmar <dassa1@seznam.cz>
2023-09-26 15:04:38 +0200

    zaklad purple task

---
+++ cypress/integration/task/Testcase1.js
@@ -0,0 +1,11 @@
+// cypress - Spec
+
+/// <reference types="Cypress" />
+
+describe('Test Case 1', function()      // popis a funkce
+{
+    it('Test case 1', function() {
+        cy.visit('https://revolgy-forms-case-study-master.staging.axiory.com/jp/registration/demo')
+
+    })
+})
